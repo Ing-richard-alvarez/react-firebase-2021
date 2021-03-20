96dfe6106193c3ba73f2ef97b84f6a8029e704dc
Richard Alvarez Salazar <ralvarez@bv.studio>
2021-03-20 01:02:46 -0500

    the project has been configured with firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 
 import React from 'react';
+import Inicio from './components/Inicio'
+import Admin from './components/Admin'
+import Login from './components/Login'
+import Menu from './components/Menu'
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 
 const App = () => {
   
   return(
-    <div>
-
+    <div className="container">
+        <Router>
+            <Menu></Menu>
+            <Switch>
+                <Route exact path="/" component={Inicio}></Route>
+                <Route  path="/admin" component={Admin}></Route>
+                <Route  path="/login" component={Login}></Route>
+            </Switch>
+        </Router>
     </div>
   );
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap'
 
 
 ReactDOM.render(
